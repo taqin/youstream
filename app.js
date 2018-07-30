7d24e3cbf05c7dfd2cf46d569679eda185aac87e
Taqin <taqin@megentics.com>
2018-07-30 21:32:36 +0800

    Modify UI

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 app.use(cookieParser());
 app.use('/arena', arena)
 
